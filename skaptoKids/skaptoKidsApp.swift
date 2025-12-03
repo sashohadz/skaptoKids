2c89a5738565c280d68c8fd32404bce17fdad06f
skapto <sasho.hadzhiev@yahoo.com>
2025-12-03 21:33:18 +0200

    Initial Commit

---
+++ skaptoKids/skaptoKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  skaptoKidsApp.swift
+//  skaptoKids
+//
+//  Created by Sasho Hadzhiev on 3.12.25.
+//
+
+import SwiftUI
+
+@main
+struct skaptoKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
